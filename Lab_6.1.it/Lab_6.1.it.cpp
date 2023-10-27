7826055b9e3348cd0057666828cfa8cd4175d3e5
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-10-28 00:34:44 +0300

    first commit

---
+++ Lab_6.1.it/Lab_6.1.it.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create(int* b, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		b[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* b, const int size)
+{
+	cout << "[";
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << b[i];
+	cout << "]" << endl;
+}
+void Find(int* b, const int size)
+{
+	for (int i = 0; i < size; i++)
+		if ((b[i] % 2 != 0 && b[i] % 3 == 0))
+			b[i] = 0;
+}
+int Sum(int* b, const int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (!(b[i] % 2 != 0 && b[i] % 3 == 0))
+		sum += b[i];
+	}
+	return sum;
+}
+int Count(int* b, const int size)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)		
+		if (!(b[i] % 2 != 0 && b[i] % 3 == 0))
+		{
+			count++;
+		}	
+	return count;
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 5;
+	int b[n];
+
+	int Low = 10;
+	int High = 90;
+	Create(b, n, Low, High);
+	Print(b, n);
+	
+	cout << Sum(b, n) << endl;
+	cout << Count(b, n) << endl;
+	Find(b, n);
+	Print(b, n);
+
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_6.1.it/Lab_6.1.it.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+int Sum(int* b, const int size);
+
+namespace UnitTestProject
+{
+    TEST_CLASS(SumFunctionTest)
+    {
+    public:
+
+        TEST_METHOD(TestSumFunction)
+        {
+            int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int size1 = 10;
+            int result1 = Sum(arr1, size1);
+            int expected1 = 43;
+            Assert::AreEqual(expected1, result1);
+
+           int arr2[] = {12, 15, 18, 21, 24};
+            int size2 = 5;
+            int result2 = Sum(arr2, size2);
+            int expected2 = 54; // Всі елементи задовольняють умову (подільні на 3)
+            Assert::AreEqual(expected2, result2);
+
+            int arr3[] = { 11, 13, 17, 19, 23 };
+            int size3 = 5;
+            int result3 = Sum(arr3, size3);
+            int expected3 = 83; // Всі елементи не задовольняють умову (не подільні на 2 та 3)
+            Assert::AreEqual(expected3, result3);
+        }
+    };
+}
